18e1359e5c76a23009056fba7090190d562aa72f
MJP <mprerau@bwh.harvard.edu>
2022-02-19 09:19:39 -0500

    Added hotcoolmap and updated redbluemap for proper interpolation

---
+++ hotcoolmap.m
@@ -0,0 +1,54 @@
+function map = hotcoolmap(N, trim_pct)
+%HOTCOOLMAP  Creates a hot/cool colormap
+%
+%   Usage:
+%   map = hotcoolmap(N, trim_pct)
+%
+%   Input:
+%   N: number of points (default: 1024)
+%   trim_pct: percent to trim to differentiate +- colors (default: 0.2)
+%
+%   Output:
+%   map: Nx3 colormap
+%
+%   Example:
+%
+%     figure
+%     imagesc(randn(1000));
+%     hotcoolmap(redbluemap);
+%     caxis([-3 3]); %Must set +- caxes to be equal magnitude to center on zero
+%
+%   Copyright 2022 Michael J. Prerau, Ph.D. - http://www.sleepEEG.org
+%   This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+%   (http://creativecommons.org/licenses/by-nc-sa/4.0/)
+%
+%   Last modified 02/19/2022
+%********************************************************************
+%Set default size
+if nargin < 1
+    N = 1024;
+end
+
+%Define trim percentage, which removes dark colors from the end which are
+%hard to distinguish from positive vs. negative
+if nargin<2
+    trim_pct = .2;
+end
+
+%Create hot and cool colormaps
+baseline_size = 1000; %size of maps from which to interpolate
+
+hot_map = hot(baseline_size);
+cool_map = hot_map(:,[2 3 1]);
+hotcool_map = [cool_map; flipud(hot_map)];
+
+trim_size = ceil(trim_pct*baseline_size);
+
+%Trim the map
+map_raw = hotcool_map(trim_size:end-trim_size,:);
+Nr = size(map_raw,1);
+
+%Interpolate to correct size
+map(:,1) = interp1(linspace(0,1,Nr),map_raw(:,1), linspace(0,1,N));
+map(:,2) = interp1(linspace(0,1,Nr),map_raw(:,2), linspace(0,1,N));
+map(:,3) = interp1(linspace(0,1,Nr),map_raw(:,3), linspace(0,1,N));
--- redbluemap.m
+++ redbluemap.m
@@ -1,90 +1,57 @@
-% Red and blue color map
-function col = redbluemap(N,wp)
+function map = redbluemap(N, white_prct)
+%REDBLUEMAP  Creates a red blue colormap
+%
+%   Usage:
+%   map = redbluemap(N, white_prct)
+%
+%   Input:
+%   N: number of points (default: 1024)
+%   white_prct: percent white in the middle (default: 0.01)
+%
+%   Output:
+%   map: Nx3 colormap
+%
+%   Example:
+%
+%     figure
+%     imagesc(randn(1000));
+%     colormap(redbluemap);
+%     caxis([-3 3]); %Must set +- caxes to be equal magnitude to center on zero
+%
+%   Copyright 2022 Michael J. Prerau, Ph.D. - http://www.sleepEEG.org
+%   This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+%   (http://creativecommons.org/licenses/by-nc-sa/4.0/)
+%
+%   Last modified 02/19/2022
+%********************************************************************
 
+%Set default N
 if nargin<1
     N=1024;
 end
 
+%Set default percentage of white
 if nargin<2
-    wp=.01;
+    white_prct=.01;
 end
 
-
+%Create the blue and red components
 blue_part=[linspace(1,0,N)',linspace(1,0,N)',ones(N,1)];
 red_part=[ones(N,1),linspace(0,1,N)',linspace(0,1,N)'];
 
-if wp==0
-    wN=1;
+%Create the white component
+if white_prct==0
+    num_white=1;
 else
-    wN=round((wp*2*N)/(1-wp));
+    num_white=round((white_prct*2*N)/(1-white_prct));
 end
-white_part=ones(wN,3);
+white_part=ones(num_white,3);
 
-col=[red_part; white_part; blue_part];
+%Create the full colormap
+map_raw=[red_part; white_part; blue_part];
+Nr = size(map_raw,1);
 
-%
-%
-% col = [         0         0    1.0000
-%     0.0323    0.0323    1.0000
-%     0.0645    0.0645    1.0000
-%     0.0968    0.0968    1.0000
-%     0.1290    0.1290    1.0000
-%     0.1613    0.1613    1.0000
-%     0.1935    0.1935    1.0000
-%     0.2258    0.2258    1.0000
-%     0.2581    0.2581    1.0000
-%     0.2903    0.2903    1.0000
-%     0.3226    0.3226    1.0000
-%     0.3548    0.3548    1.0000
-%     0.3871    0.3871    1.0000
-%     0.4194    0.4194    1.0000
-%     0.4516    0.4516    1.0000
-%     0.4839    0.4839    1.0000
-%     0.5161    0.5161    1.0000
-%     0.5484    0.5484    1.0000
-%     0.5806    0.5806    1.0000
-%     0.6129    0.6129    1.0000
-%     0.6452    0.6452    1.0000
-%     0.6774    0.6774    1.0000
-%     0.7097    0.7097    1.0000
-%     0.7419    0.7419    1.0000
-%     0.7742    0.7742    1.0000
-%     0.8065    0.8065    1.0000
-%     0.8387    0.8387    1.0000
-%     0.8710    0.8710    1.0000
-%     0.9032    0.9032    1.0000
-%     0.9355    0.9355    1.0000
-%     0.9677    0.9677    1.0000
-%     1.0000    1.0000    1.0000
-%     1.0000    0.9688    0.9688
-%     1.0000    0.9375    0.9375
-%     1.0000    0.9062    0.9062
-%     1.0000    0.8750    0.8750
-%     1.0000    0.8438    0.8438
-%     1.0000    0.8125    0.8125
-%     1.0000    0.7812    0.7812
-%     1.0000    0.7500    0.7500
-%     1.0000    0.7188    0.7188
-%     1.0000    0.6875    0.6875
-%     1.0000    0.6562    0.6562
-%     1.0000    0.6250    0.6250
-%     1.0000    0.5938    0.5938
-%     1.0000    0.5625    0.5625
-%     1.0000    0.5312    0.5312
-%     1.0000    0.5000    0.5000
-%     1.0000    0.4688    0.4688
-%     1.0000    0.4375    0.4375
-%     1.0000    0.4062    0.4062
-%     1.0000    0.3750    0.3750
-%     1.0000    0.3438    0.3438
-%     1.0000    0.3125    0.3125
-%     1.0000    0.2812    0.2812
-%     1.0000    0.2500    0.2500
-%     1.0000    0.2188    0.2188
-%     1.0000    0.1875    0.1875
-%     1.0000    0.1562    0.1562
-%     1.0000    0.1250    0.1250
-%     1.0000    0.0938    0.0938
-%     1.0000    0.0625    0.0625
-%     1.0000    0.0312    0.0312
-%     1.0000         0         0];
+%Interpolate to correct size
+map(:,1) = interp1(linspace(0,1,Nr), map_raw(:,1), linspace(0,1,N));
+map(:,2) = interp1(linspace(0,1,Nr), map_raw(:,2), linspace(0,1,N));
+map(:,3) = interp1(linspace(0,1,Nr), map_raw(:,3), linspace(0,1,N));
